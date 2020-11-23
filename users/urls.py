ef1329c75dcf09e4fd8c2611896f9b4f6e2fd664
Jessica Weins <j.lynn.weins@gmail.com>
2020-11-22 18:14:00 -0700

    Adding in user authentication

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,10 @@
 from django.urls import path, include
 
 app_name = 'users'
-urlpattern = [
+urlpatterns = [
         # Include default auth urls
         path('',include('django.contrib.auth.urls')),
+        # Registration page
+        path('register/', views.register, name='register'),
+        
         ]
